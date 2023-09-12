605655b60da95c237d0ccf5cf476d30cb080b29a
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2023-09-12 12:48:02 +0300

    delete imports

--- src/main/java/by/touchme/authservice/handler/ErrorHandler.java
+++ src/main/java/by/touchme/authservice/handler/ErrorHandler.java
@@ -1,25 +1,15 @@
 package by.touchme.authservice.handler;
 
 import by.touchme.authservice.exception.AuthException;
-import io.jsonwebtoken.ExpiredJwtException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Controller that catches all thrown exceptions.
